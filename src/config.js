272826621d58c9c960b64fcdaa0c29e593850a83
Khuziama Rehman <rehmankhuziama@gmail.com>
2022-05-19 17:55:24 -0400

    base url react

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const axios = require('axios').default;
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
-    baseURL: API_BASE_URL,
+	baseURL: API_BASE_URL
 });
 
 module.exports = axiosInstance;
