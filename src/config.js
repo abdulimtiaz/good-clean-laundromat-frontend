eeebde53e976fee683f1efd9829ac108a33647b4
abdulimtiaz <abdulimtiaz98@gmail.com>
2022-05-19 17:50:07 -0400

    changed env variable name

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const axios = require('axios').default;
 
-const API_BASE_URL = process.env.API_BASE_URL || 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
